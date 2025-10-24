eafe7cf4576d25fca5ad61d06f2912e866e25749
justMSARahat <justshamsulalom@gmail.com>
2025-10-25 00:40:31 +0600

    25 Oct

---
+++ CSE-3112 | Object Oriented Programming Sessional/Lecture_2/src/Basics/Student.java
@@ -0,0 +1,36 @@
+package Basics; // Folder Structure
+
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args){
+        // int a;
+        // int b;
+        // float c;
+        Scanner sc = new Scanner(System.in);
+
+        // a = sc.nextInt();
+        // a = sc.nextInt();
+        // b = sc.nextInt();
+        // c = sc.nextFloat();
+
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        // char ch;
+        // ch = sc.next().charAt(0);
+
+        // System.out.println(ch);
+        // System.out.println("-----------------------");
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+
+    }
+}
